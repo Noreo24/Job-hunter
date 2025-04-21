f272a60bc1eabf1a50c7739b4fb91af63fb37787
Noreo <buianhtuan.kbhb24@gmail.com>
2025-04-22 00:24:37 +0700

    00h24 - 22/04/2025 - Delete + get + update user

--- src/main/java/vn/noreo/jobhunter/controller/UserController.java
+++ src/main/java/vn/noreo/jobhunter/controller/UserController.java
@@ -5,8 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.noreo.jobhunter.domain.User;
 import vn.noreo.jobhunter.service.UserService;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -17,9 +24,31 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User newUser) {
         User user = this.userService.handleCreateUser(newUser);
+        return user;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "Deleted user with id: " + id;
+    }
+
+    @GetMapping("/user/{id}")
+    public User fetchUserById(@PathVariable("id") long id) {
+        return this.userService.handleFetchUserById(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> fetchAllUser() {
+        return this.userService.handleFetchAllUser();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User updatedUser) {
+        User user = this.userService.handleUpdateUser(updatedUser);
         return user;
     }
 }
--- src/main/java/vn/noreo/jobhunter/service/UserService.java
+++ src/main/java/vn/noreo/jobhunter/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.noreo.jobhunter.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import vn.noreo.jobhunter.domain.User;
@@ -16,5 +18,30 @@ public class UserService {
 
     public User handleCreateUser(User newUser) {
         return this.userRepository.save(newUser);
+    }
+
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public User handleFetchUserById(long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> handleFetchAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User updatedUser) {
+        User currentUser = this.handleFetchUserById(updatedUser.getId());
+        if (currentUser != null) {
+            currentUser.setName(updatedUser.getName());
+            currentUser.setEmail(updatedUser.getEmail());
+            currentUser.setPassword(updatedUser.getPassword());
+
+            currentUser = this.userRepository.save(currentUser);
+        }
+        return currentUser;
+        // return this.userRepository.save(updatedUser);
     }
 }
